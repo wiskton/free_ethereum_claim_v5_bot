401ca0d0545d274483948ceb0c5204527f662d55
Willem Allan <willemarf@gmail.com>
2021-07-01 09:52:21 -0300

    ajust

--- click_miner.py
+++ click_miner.py
@@ -3,13 +3,13 @@ import time
 from pyautogui import press, typewrite, hotkey
 
 delay = 1801
+group_eth_claim_x = 100
+group_eth_claim_y = 100
+
+button_claim_x = 540
+button_claim_y = 430
 
 def gain():
-    group_eth_claim_x = 100
-    group_eth_claim_y = 100
-
-    button_claim_x = 540
-    button_claim_y = 430
 
     mouse.move(group_eth_claim_x, group_eth_claim_y, absolute=True, duration=0.1)
     mouse.click('left')
